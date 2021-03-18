5c21a6420048f663afb4a6bff189de1cd846e793
StuBehan <33100049+StuBehan@users.noreply.github.com>
2021-03-18 13:24:21 +0000

    updated test

--- spec/features/attack_reduce_hp_spec.rb
+++ spec/features/attack_reduce_hp_spec.rb
@@ -3,6 +3,7 @@ describe Battle do
     scenario 'displays the reduced hp following a player attack' do
       sign_in_and_play
       click_button "attack"
+      click_button "Return"
       expect(page).to have_content("9hp")
     end
   end
